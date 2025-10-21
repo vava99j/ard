d8cf6fc1c68167b72a6e20dbca3b8eed96b6f1b0
Vagner Jorge Souza Feitosa <svagnerjorge@gmail.com>
2025-10-21 16:42:47 -0300

    Update ard.js

--- ard-back/ard.js
+++ ard-back/ard.js
@@ -1,12 +1,9 @@
 import { SerialPort } from 'serialport';
 import Arduino from "./comp.js";
 
-
 SerialPort.list().then(ports => {
   ports.forEach(port => {
     console.log(`Porta: ${port.path}`);
-    Arduino(port.path)
+    Arduino(port.path);
   });
-  
 });
-
